555805eaefdf68640e0f94a3974895319da36330
Jay Mills <jay_michael_mills@live.com>
2025-12-11 07:52:43 -0500

    comments-TODO

--- Build_Builder/MainPlayer.h
+++ Build_Builder/MainPlayer.h
@@ -3,20 +3,29 @@
 // This will be the parent that sub classes derive from
 // 
 
-class MainPlayer
+class PlayerBase
 {
 private:
 	std::string class_name;
 protected:
 	//Base Information
 	std::string name;
-	int char_level;
-
+	
+	//Leveling
+	//========= INTS ===============================================
+	int char_level,
+	//Level stats
+		strength,
+		intelligence,
+		dexterity,
+		vitality, //HealthPoints
+		wisdom; //ManaPool
 
 	//========= Doubles =========================================================================================//
 	double
 		//Base stats
 		//Health Defence
+		mana_points, //Amount of Mana
 		health_points, //Amount of Health
 		armor, //Rolls for amount of damage negated
 		evasion_chance, //Rolls for change to dodge attack and negate 100% of damage
@@ -28,13 +37,15 @@ protected:
 
 		//Elemental
 			//Attack
-		base_fire,
-		base_cold,
-		base_lightning,
-		base_blood,
-		base_dark,
+			//Elemental attacks can cause DOT(Damage Over Time)
+		base_fire, //Burn
+		base_cold, //Chill
+		base_lightning, //Shock
+		base_blood, //Bleed
+		base_dark, //Cursed
 
 		//Resistance
+		//This will block initial elem damage
 		base_fire_resistance,
 		base_cold_resistance,
 		base_lightning_resistance,
@@ -47,8 +58,22 @@ protected:
 		
 		
 	//========== End of doubles =================================================================================//
-	
+
+	//After all of the base vars are set, we can add the methods
+	//Constructors to set chars base
+	PlayerBase();
+
+	//Getters
+
+	//Setters
+
+	//Methods
+
 
 public:
 
 };
+
+//As we add more classes, we can set randomized start values or 
+//values that we can multiply with depending on the class.
+//ie; Armor and strength are higher with tanks but lower with rougues/archers/mages
